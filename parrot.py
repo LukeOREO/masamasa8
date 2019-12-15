dc8594824cbeb8a1be808fba197d9156fa286012
LukeOREO <masa52105@yahoo.co.jp>
2019-12-15 23:53:11 +0900

    a

--- parrot.py
+++ parrot.py
@@ -61,11 +61,11 @@ def handle_image(event):
 
      # 画像の送信
     image_message = ImageSendMessage(
-        original_content_url=f"https://date-the-image.herokuapp.com/{main_image_path}",
-        preview_image_url=f"https://date-the-image.herokuapp.com/{main_image_path}",
+        original_content_url=f"https://parrotaro.herokuapp.com/{main_image_path}",
+        preview_image_url=f"https://parrotaro.herokuapp.com/{main_image_path}",
     )
 
-    app.logger.info(f"https://date-the-image.herokuapp.com/{main_image_path}")
+    app.logger.info(f"https://parrotaro.herokuapp.com/{main_image_path}")
     line_bot_api.reply_message(event.reply_token, image_message)
 
     # 画像を削除する
