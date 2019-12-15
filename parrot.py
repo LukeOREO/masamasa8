2aced4329800576201ce5af6353e7bbe4e7f2939
LukeOREO <masa52105@yahoo.co.jp>
2019-12-15 23:29:26 +0900

    a

--- parrot.py
+++ parrot.py
@@ -1,3 +1,7 @@
+import os
+from pathlib import Path
+from typing import List
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -6,10 +10,9 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-import os
+
+from linebot.models import (ImageMessage, ImageSendMessage, MessageEvent,
+                            TextMessage, TextSendMessage
 
 app = Flask(__name__)
 app.debug = False
@@ -20,6 +23,10 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+                            
+SRC_IMAGE_PATH = "static/images/{}.jpg"
+MAIN_IMAGE_PATH = "static/images/{}_main.jpg"
+PREVIEW_IMAGE_PATH = "static/images/{}_preview.jpg"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -39,17 +46,26 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    if event.reply_token == "00000000000000000000000000000000":
-        return
-    
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image(event):
+    message_id = event.message.id
+
+    src_image_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
+    main_image_path = MAIN_IMAGE_PATH.format(message_id)
+    preview_image_path = PREVIEW_IMAGE_PATH.format(message_id)
+
+    # 画像を保存
+    save_image(message_id, src_image_path)
 
 
-if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+     # 画像の送信
+    image_message = ImageSendMessage(
+        original_content_url=f"https://date-the-image.herokuapp.com/{main_image_path}",
+        preview_image_url=f"https://date-the-image.herokuapp.com/{main_image_path}",
+    )
+
+    app.logger.info(f"https://date-the-image.herokuapp.com/{main_image_path}")
+    line_bot_api.reply_message(event.reply_token, image_message)
+
+    # 画像を削除する
+    src_image_path.unlink()
